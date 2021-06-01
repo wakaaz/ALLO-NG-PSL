95cbfc659716a21cf1e8ddf516ea65fd32503a06
zubaid.rasool <web.expert1221@gmail.com>
2021-06-02 01:14:46 +0500

    Video download feature enabled on Video Player page

--- src/app/components/player/player.module.ts
+++ src/app/components/player/player.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { PlayerRoutingModule } from './player-routing.module';
 import { PlayerComponent } from './player.component';
 import { PlyrModule } from 'ngx-plyr';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [PlayerComponent],
   imports: [
     CommonModule,
+    FormsModule,
     PlyrModule,
     PlayerRoutingModule
   ]
