76fc4f2f960702a45fa0046297d2f9885b97935a
M. Ahsan Jutt <ahsanjutt0999@outlook.com>
2021-06-01 21:54:29 +0500

    player done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
   production: false,
   ENV: 'development',
   // API_URL: 'http://192.168.1.115/vape/public',
+  // API_URL: 'https://admin.aajkaadin.com/api/',
   API_URL: 'http://psl.am7.studio',
   APP_NAME: 'PSL | '
 };
